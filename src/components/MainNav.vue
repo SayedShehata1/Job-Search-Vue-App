211d889b308de36660924ac90d3b864209738b4f
Sayed <sayedshehata33@gmail.com>
2024-07-09 11:46:13 +0300

    feat: :sparkles: Add Navigation menu with tests

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,14 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex h-full px-8 mx-auto border-b border-solid flex-nowrap border-brand-gray-1">
-        <a class="flex items-center h-full text-xl" href="/">{{ company }}</a>
+        <a :href="url" class="flex items-center h-full text-xl">{{ company }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full list-none">
+            <li v-for="menuItem in menuItems" :key="menuItem" class="h-full ml-9 first:ml-0">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -13,7 +20,9 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Sayed Careers'
+      company: 'Google Careers',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Location', 'Life at Google', 'How we hire', 'Students', 'Jobs']
     }
   }
 }
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,7 +8,26 @@ describe('MainNav', () => {
     render(MainNav)
 
     // screen.getByText: find the element with the text 'Sayed Careers'
-    const companyName = screen.getByText('Sayed Careers')
+    const companyName = screen.getByText('Google Careers')
     expect(companyName).toBeInTheDocument()
+  })
+  it('displays menu items for navigation', () => {
+    render(MainNav)
+
+    // screen.getAllByRole: find all elements with the role:key 'listitem'
+    const navigationMenuItems = screen.getAllByRole('listitem')
+
+    // map over the navigationMenuItems and get the textContent of each item
+    const navigationMenuText = navigationMenuItems.map((item) => item.textContent)
+
+    // expect the textContent of the navigationMenuItems to be equal to the array of strings (with array we can use toEqual no toBe)
+    expect(navigationMenuText).toEqual([
+      'Teams',
+      'Location',
+      'Life at Google',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
   })
 })
