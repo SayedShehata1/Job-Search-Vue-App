b6d9ed6ea589063a42655057bc46746b32e705db
Sayed <sayedshehata33@gmail.com>
2024-07-14 00:50:58 +0300

    feat: :sparkles: Add Job Search Form

---
+++ src/components/JobSearch/JobSearchForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form class="flex items-center w-full h-12 border border-solid rounded-3xl border-brand-gray-3">
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex flex-1 h-full text-base font-light flex-nowrap">
+      <div class="relative flex items-center flex-1 h-full pr-3">
+        <label class="absolute left-0 -top-10">Role</label>
+        <input
+          v-model="role"
+          type="text"
+          class="w-full text-lg font-normal focus:outline-none"
+          placeholder="Search for jobs"
+          s
+        />
+      </div>
+      <span
+        class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
+        >in</span
+      >
+      <div class="relative flex items-center flex-1 h-full pl-3">
+        <label class="absolute left-0 -top-10">Where?</label>
+        <input
+          v-model="location"
+          type="text"
+          class="w-full text-lg font-normal focus:outline-none"
+          placeholder="Cairo, Egypt"
+        />
+      </div>
+    </div>
+    <action-button text="Search" type="secondary" class="rounded-r-3xl" />
+  </form>
+</template>
+
+<script>
+import ActionButton from '@/components/Shared/ActionButton.vue'
+export default {
+  name: 'JobSearchForm',
+  components: {
+    ActionButton
+  },
+  data() {
+    return {
+      role: '',
+      location: ''
+    }
+  }
+}
+</script>
