3d1c3e7673f5026103e05fad33180a4d42985875
Sayed <sayedshehata33@gmail.com>
2024-07-17 20:12:31 +0300

    feat: :alien: Handle Jobs with a mock data from json server

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,17 +1,23 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-solid rounded border-brand-gray-2 hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
-        <h2>Technical Program Manger, Perception, Augmented Reality</h2>
+        <h2>{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Google</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>San Francisco, CA, USA</span>
+            <ul>
+              <li v-for="location in job.locations" :key="location" class="inline-block mr-5">
+                <span>
+                  {{ location }}
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -20,17 +26,14 @@
           <h3>Qualifications</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bachelor's degree or equivalent management</li>
-              <li>5 years of experience in program management</li>
-              <li>
-                Experience analyzing data through querying database (e.g.,SQL) using spreadsheet
-                software, and creating statistical models.
+              <li v-for="qualification in job.minimumQualifications" :key="qualification">
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1">Expand</router-link>
+          <router-link :to="jobPageLink" class="text-brand-blue-1">Expand</router-link>
         </div>
       </div>
     </router-link>
@@ -39,6 +42,17 @@
 
 <script>
 export default {
-  name: 'JobListing'
+  name: 'JobListing',
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    jobPageLink() {
+      return `/job/results/${this.job.id}`
+    }
+  }
 }
 </script>
