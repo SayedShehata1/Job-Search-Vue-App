90f4ba4b118b4f327fa4f46eb87a683cdef2c999
Sayed <sayedshehata33@gmail.com>
2024-07-19 21:25:04 +0300

    feat: :sparkles: Create Collapsible Accordion component using Slots

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1">
-    Job Filter Sidebar
+  <div class="flex flex-col p-4 bg-white border-r border-solid w-96 border-brand-gray-1">
+    <section>
+      <div class="pb-5">
+        <div class="flex justify-between">
+          <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+          <div class="flex items-center text-sm">
+            <action-button text="Clear Filters" type="secondary"></action-button>
+          </div>
+        </div>
+      </div>
+      <collapsible-accordion header="Degree"> </collapsible-accordion>
+      <collapsible-accordion header="Job Type"> hello </collapsible-accordion>
+
+      <job-filter-sidebar-organizations />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from '@/components/Shared/ActionButton.vue'
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+import JobFilterSidebarOrganizations from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue'
+
 export default {
-  name: 'JobFilterSidebar'
+  name: 'JobFilterSidebar',
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+    JobFilterSidebarOrganizations
+  }
 }
 </script>
