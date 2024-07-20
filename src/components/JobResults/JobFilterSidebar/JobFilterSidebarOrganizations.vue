de5a182dca319e8d3cf5719b4a08391d88d9747e
Sayed <sayedshehata33@gmail.com>
2024-07-20 22:38:24 +0300

    feat: :sparkles: Handle Filter with the global state

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue
@@ -3,21 +3,16 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="w-1/2 h-8">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Between Vue" type="checkbox" class="mr-3" />
-            <label for="Between Vue">Between Vue</label>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-            <label for="Et Vue Brute">Et Vue Brute</label>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-            <label for="Vue and a Half Men">Vue and a Half Men</label>
+          <li v-for="Organization in UNIQUE_ORGANIZATIONS" :key="Organization" class="w-1/2 h-8">
+            <input
+              :id="Organization"
+              v-model="selectedOrganizations"
+              :value="Organization"
+              type="checkbox"
+              class="mr-3"
+              @change="selectOrganization"
+            />
+            <label :for="Organization">{{ Organization }}</label>
           </li>
         </ul>
       </fieldset>
@@ -26,11 +21,34 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'pinia'
+import { useJobsStore, UNIQUE_ORGANIZATIONS } from '@/stores/jobs'
+import { useUserStore, ADD_SELECTED_ORGANIZATIONS } from '@/stores/user'
+
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
 export default {
   name: 'JobFilterSidebarOrganizations',
   components: {
     CollapsibleAccordion
+  },
+  data() {
+    return {
+      selectedOrganizations: []
+    }
+  },
+  computed: {
+    //  with the mapState helper we can map the store state to the component's computed properties
+    // first argument is the store we want to map to, and the second argument is an array of the store's (state or getter )we want to map
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS])
+  },
+  methods: {
+    // with the mapActions helper we can map the store actions to the component's methods
+    ...mapActions(useUserStore, [ADD_SELECTED_ORGANIZATIONS]),
+
+    // this method is called when the user selects an organization to filter by and it adds the selected organization to the user store
+    selectOrganization() {
+      this.ADD_SELECTED_ORGANIZATIONS(this.selectedOrganizations)
+    }
   }
 }
 </script>
