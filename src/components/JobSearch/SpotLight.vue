04f6c5a945153364ecf78073504baeb304519c77
Sayed <sayedshehata33@gmail.com>
2024-07-20 14:47:09 +0300

    feat: :sparkles: Add Spotlight component using slot props to make it dynamic

---
+++ src/components/JobSearch/SpotLight.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot
+        :img="spotlight.img"
+        :title="spotlight.title"
+        :description="spotlight.description"
+      ></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SpotLight',
+  data() {
+    return {
+      spotlights: []
+    }
+  },
+  async mounted() {
+    const baseUrl = import.meta.env.VITE_API_URL
+    const response = await axios.get(`${baseUrl}/spotlights`)
+    this.spotlights = response.data
+    console.log(this.spotlights)
+  }
+}
+</script>
