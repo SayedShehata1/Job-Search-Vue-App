df25c1842ab6350e0f2be8cbb511c3c2d4fb7509
Sayed <sayedshehata33@gmail.com>
2024-07-23 19:44:31 +0300

    feat: :sparkles: Make JobFilterPrompt Component

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -2,16 +2,7 @@
   <div class="flex flex-col p-4 bg-white border-r border-solid w-96 border-brand-gray-1">
     <section>
       <div class="pb-5">
-        <div class="flex justify-between">
-          <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-          <div class="flex items-center text-sm">
-            <action-button
-              text="Clear Filters"
-              type="secondary"
-              @click="CLEAR_USER_JOB_FILTER_SELECTIONS"
-            ></action-button>
-          </div>
-        </div>
+        <job-filter-sidebar-prompt />
       </div>
       <collapsible-accordion header="Degrees">
         <job-filter-sidebar-degrees />
@@ -27,17 +18,10 @@
 </template>
 
 <script lang="ts" setup>
-import ActionButton from '@/components/Shared/ActionButton.vue'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
 
 import JobFilterSidebarDegrees from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarDegrees.vue'
 import JobFilterSidebarJobs from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarJobs.vue'
 import JobFilterSidebarOrganizations from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue'
-
-import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
-
-// action to clear user job filter selections
-const CLEAR_USER_JOB_FILTER_SELECTIONS = userStore.CLEAR_USER_JOB_FILTER_SELECTIONS
+import JobFilterSidebarPrompt from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarPrompt.vue'
 </script>
---
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarPrompt.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-between">
+    <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+    <div class="flex items-center text-sm">
+      <action-button
+        text="Clear Filters"
+        type="secondary"
+        @click="CLEAR_USER_JOB_FILTER_SELECTIONS"
+      ></action-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ActionButton from '@/components/Shared/ActionButton.vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+
+// action to clear user job filter selections
+const CLEAR_USER_JOB_FILTER_SELECTIONS = userStore.CLEAR_USER_JOB_FILTER_SELECTIONS
+</script>
