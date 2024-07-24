a671286e0c840ac5a3882552297532f8038a500b
Sayed <sayedshehata33@gmail.com>
2024-07-24 16:22:13 +0300

    feat: :sparkles: Add JobFilterSkills Component

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="flex flex-col p-4 bg-white border-r border-solid w-96 border-brand-gray-1">
-    <section>
-      <div class="pb-5">
-        <job-filter-sidebar-prompt />
-      </div>
+    <section class="pb-5">
+      <job-filter-sidebar-prompt />
+
+      <job-filtere-sidebar-skills />
+
       <collapsible-accordion header="Degrees">
         <job-filter-sidebar-degrees />
       </collapsible-accordion>
+
       <collapsible-accordion header="Job Types">
         <job-filter-sidebar-jobs />
       </collapsible-accordion>
+
       <collapsible-accordion header="Organizations">
         <job-filter-sidebar-organizations />
       </collapsible-accordion>
@@ -24,4 +27,5 @@ import JobFilterSidebarDegrees from '@/components/JobResults/JobFilterSidebar/Jo
 import JobFilterSidebarJobs from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarJobs.vue'
 import JobFilterSidebarOrganizations from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue'
 import JobFilterSidebarPrompt from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarPrompt.vue'
+import JobFiltereSidebarSkills from '@/components/JobResults/JobFilterSidebar/JobFilterSidebarSkills.vue'
 </script>
---
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarSkills.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-2">
+    <input
+      v-model="skillsSearchTerm"
+      type="text"
+      class="w-full h-12 p-3 text-base border-solid rounded border-brand-gray-1 shadow-gray"
+      placeholder="Computer programming, Finance degree"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/user'
+import { computed } from 'vue'
+
+const userStore = useUserStore()
+
+const skillsSearchTerm = computed({
+  get() {
+    return userStore.skillsSearchTerm
+  },
+  set(value: string) {
+    userStore.UPDATE_SKILLS_SEARCH_TERM(value)
+  }
+})
+</script>
