21521d60f7ccc1948a6f407586633cd3252af4f7
Sayed <sayedshehata33@gmail.com>
2024-07-24 16:29:17 +0300

    feat: :zap: Make the Search lazy and trim the Input

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebarSkills.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarSkills.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-2">
     <input
-      v-model="skillsSearchTerm"
+      v-model.lazy.trim="skillsSearchTerm"
       type="text"
       class="w-full h-12 p-3 text-base border-solid rounded border-brand-gray-1 shadow-gray"
       placeholder="Computer programming, Finance degree"
@@ -15,10 +15,13 @@ import { computed } from 'vue'
 
 const userStore = useUserStore()
 
+// Computed property to get and set the skills search term
 const skillsSearchTerm = computed({
+  // Getter function to get the value of the skills search term
   get() {
     return userStore.skillsSearchTerm
   },
+  // Setter function to set the value of the skills search term
   set(value: string) {
     userStore.UPDATE_SKILLS_SEARCH_TERM(value)
   }
