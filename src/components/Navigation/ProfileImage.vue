fcbab15bbe5fa4cdb94740264c996e4927fa5142
Sayed <sayedshehata33@gmail.com>
2024-07-27 12:58:47 +0300

    feat: :bento: Update user image

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -5,6 +5,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 const imageLink = ref(
-  'https://images.unsplash.com/photo-1535585105038-bf206e97d068?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  'https://images.unsplash.com/photo-1485206412256-701ccc5b93ca?q=80&w=1794&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 )
 </script>
